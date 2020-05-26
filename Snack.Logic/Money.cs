718b054bf22e3b9efdab289c98d8795b3b19b08a
Sergi Virgili <svirgilif@gmail.com>
2020-05-26 21:00:04 +0200

    Add Money Value Object

--- Snack.Logic/Money.cs
+++ Snack.Logic/Money.cs
@@ -1,10 +1,67 @@
 ﻿using System;
 namespace Snack.Logic
 {
-    public class Money
+    public sealed class Money : ValueObject<Money>
     {
-        public Money()
+        public int OneCentCount { get; private set; }
+        public int TenCentCount { get; private set; }
+        public int QuarterCount { get; private set; }
+        public int OneDollarCount { get; private set; }
+        public int FiveDollarCount { get; private set; }
+        public int TwentyDollarCount { get; private set; }
+
+        public Money(
+            int oneCentCount,
+            int tenCentCount,
+            int quarterCount,
+            int oneDollarCount,
+            int fiveDollarCount,
+            int twentyDollarCount)
         {
+            OneCentCount += oneCentCount;
+            TenCentCount += tenCentCount;
+            QuarterCount += quarterCount;
+            OneDollarCount += oneDollarCount;
+            FiveDollarCount += fiveDollarCount;
+            TwentyDollarCount += twentyDollarCount;
+        }
+
+        public static Money operator +(Money money1, Money money2)
+        {
+            Money sum = new Money(
+                money1.OneCentCount + money2.OneCentCount,
+                money1.TenCentCount + money2.TenCentCount,
+                money1.QuarterCount + money2.QuarterCount,
+                money1.OneDollarCount + money2.OneDollarCount,
+                money1.FiveDollarCount + money2.FiveDollarCount,
+                money1.TwentyDollarCount + money2.TwentyDollarCount
+                );
+            return sum;
+        }
+
+        protected override bool EqualsCore(Money other)
+        {
+            return OneCentCount == other.OneCentCount &&
+            TenCentCount == other.TenCentCount &&
+            QuarterCount == other.QuarterCount &&
+            OneDollarCount == other.OneDollarCount &&
+            FiveDollarCount == other.FiveDollarCount &&
+            TwentyDollarCount == other.TwentyDollarCount;
+        }
+
+        protected override int GetHashCodeCore()
+        {
+            unchecked
+            {
+                int hashcode = OneCentCount;
+                hashcode = (hashcode * 397) ^ TenCentCount;
+                hashcode = (hashcode * 397) ^ QuarterCount;
+                hashcode = (hashcode * 397) ^ OneDollarCount;
+                hashcode = (hashcode * 397) ^ FiveDollarCount;
+                hashcode = (hashcode * 397) ^ TwentyDollarCount;
+
+                return hashcode;
+            }
         }
     }
 }
