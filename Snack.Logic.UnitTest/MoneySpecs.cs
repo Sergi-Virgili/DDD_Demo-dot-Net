fa526c4d9241d1c37427cd1ec6b7991852035268
Sergi Virgili <svirgilif@gmail.com>
2020-05-27 12:11:51 +0200

    Test Two money intances are equals if contain same money amount

--- Snack.Logic.UnitTest/MoneySpecs.cs
+++ Snack.Logic.UnitTest/MoneySpecs.cs
@@ -22,5 +22,15 @@ namespace Snack.Logic.UnitTest
             sum.FiveDollarCount.Should().Be(10);
             sum.TwentyDollarCount.Should().Be(12);
         }
+
+        [Fact]
+        public void Two_money_instances_equal_if_contain_the_same_money_amount()
+        {
+            Money money1 = new Money(1, 2, 3, 4, 5, 6);
+            Money money2 = new Money(1, 2, 3, 4, 5, 6);
+
+            money1.Should().Be(money2);
+            money1.GetHashCode().Should().Be(money2.GetHashCode());
+        }
     }
 }
