335a1a70d75c61763012634132842aff13281425
Sergi Virgili <svirgilif@gmail.com>
2020-05-27 12:06:11 +0200

    Test sum money produces a correct result

---
+++ Snack.Logic.UnitTest/MoneySpecs.cs
@@ -0,0 +1,26 @@
+using System;
+using FluentAssertions;
+using Xunit;
+
+
+namespace Snack.Logic.UnitTest
+{
+    public class MoneySpecs
+    {
+        [Fact]
+        public void Sum_of_two_moneys_produces_a_correct_result()
+        {
+            Money money1 = new Money(1, 2, 3, 4, 5, 6);
+            Money money2 = new Money(1, 2, 3, 4, 5, 6);
+
+            Money sum = money1 + money2;
+
+            sum.OneCentCount.Should().Be(2);
+            sum.TenCentCount.Should().Be(4);
+            sum.QuarterCount.Should().Be(6);
+            sum.OneDollarCount.Should().Be(8);
+            sum.FiveDollarCount.Should().Be(10);
+            sum.TwentyDollarCount.Should().Be(12);
+        }
+    }
+}
